f4f3c21e302ddf7af6927ac7dba6b87c555477f6
Dotto <127796594+DotLYHiyou@users.noreply.github.com>
2024-04-30 20:33:37 -0400

    Create thecode.js

---
+++ thecode.js
@@ -0,0 +1,2 @@
+console.log("If you see this in the console, then the 'console.log()' in your bookmarklet is working.")
+alert("If you see this in the console, then the 'alert()' in your bookmarklet is working.")
